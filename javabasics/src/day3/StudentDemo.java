a5cef14e98ae0f87e153a5c1da4e5bafb1c90396
rakes <rakes@MSI>
2024-06-27 16:00:21 +0530

    "oop concept"

---
+++ javabasics/src/day3/StudentDemo.java
@@ -0,0 +1,23 @@
+package day3;
+
+public class StudentDemo {
+
+	public static void main(String[] args) {
+		Student s1=new Student();
+		s1.setSid(102);
+		s1.setName("raja");
+		s1.setCity("puducherry");
+		System.out.println(s1);
+		
+		
+		 System.out.println("student id:"+s1.getSid());
+		  System.out.println("student name:"+s1.getName());
+		  System.out.println("student city:"+s1.getCity());
+		 System.out.println(s1);
+		
+		
+		
+
+	}
+
+}

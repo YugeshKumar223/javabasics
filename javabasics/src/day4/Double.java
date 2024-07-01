c4c3c100fbe1fd0c4d6dfd1263adbb916fa3028f
rakes <rakes@MSI>
2024-07-01 15:59:37 +0530

    java basics

---
+++ javabasics/src/day4/Double.java
@@ -0,0 +1,12 @@
+package day4;
+
+public class Double {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double value=32;
+		System.out.println("A double "+ value);
+
+	}
+
+}
---
+++ javabasics/src/day4/UserName.java
@@ -0,0 +1,17 @@
+package day4;
+
+import java.util.Scanner;
+
+public class UserName {
+
+	public static void main(String[] args) {
+		
+		Scanner input=new Scanner(System.in);
+		System.out.println("enter your name");
+		String n=input.nextLine();
+		System.out.println("Hello "+n);  
+		
+
+	}
+
+}
